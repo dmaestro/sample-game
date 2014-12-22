b79a3eb36163451a2e771b3978daa7f37e1915bc
Douglas L. Schrag <doug@theschrags.net>
2014-12-22 09:05:02 -0500

    design for the Turn class

---
+++ lib/Yahtzee/Turn.pm
@@ -0,0 +1,57 @@
+package Yahtzee::Turn;
+use 5.014;
+use warnings;
+
+use Game::Die;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Yahtzee::Turn;
+
+=head1 SYNOPSIS
+
+    my $turn = Yahtzee::Turn->new();
+
+    say $turn->as_string;
+    # 1: 2
+    # 2: 4
+    # 3: 4
+    # 4: 5
+    # 5: 6
+    say $turn->rethrows_remaining;
+    # 2
+
+    $turn->rethrow(1,3);
+    say $turn->as_string;
+    # 1: 1
+    # 2: 4
+    # 3: 5
+    # 4: 6
+    # 5: 6
+
+    turn->rethrow(1,2,3);
+    say $turn->as_string;
+    # 1: 2
+    # 2: 6
+    # 3: 6
+    # 4: 6
+    # 5: 6
+    say $turn->rethrows_remaining;
+    # 0
+
+    say $turn->result;
+    # Four of a kind
+    say $turn->significant_value;
+    # 6
+    say $turn->spot_total;
+    # 26
+
+=head1 DESCRIPTION
+
+This class represents the rules for taking a single turn at the
+game of Yahtzee. It provides methods for viewing the individual
+dice, category matched, and other information useful for scoring.
